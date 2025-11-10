07333ed6820ac3289ff86d20cd8df94cc6139c5b
Jessica <diackjessica5@gmail.com>
2025-11-10 16:09:14 -0500

    feat: add env loader for additional env parsing

---
+++ src/main/java/com/api/kanban/Config/EnvLoader.java
@@ -0,0 +1,20 @@
+package com.api.kanban.Config;
+
+import io.github.cdimascio.dotenv.Dotenv;
+
+public class EnvLoader {
+    public static void loadEnv() {
+        Dotenv dotenv;
+        dotenv = Dotenv.configure()
+                .directory("./")
+                .filename(".env")
+                .load();
+
+        System.setProperty("SPRING_DATASOURCE_URL", dotenv.get("SPRING_DATASOURCE_URL"));
+        System.setProperty("SPRING_DATASOURCE_USERNAME", dotenv.get("SPRING_DATASOURCE_USERNAME"));
+        System.setProperty("SPRING_DATASOURCE_PASSWORD", dotenv.get("SPRING_DATASOURCE_PASSWORD"));
+        System.setProperty("SPRING_JPA_HIBERNATE_DDL_AUTO", dotenv.get("SPRING_JPA_HIBERNATE_DDL_AUTO"));
+        System.setProperty("SPRING_JPA_SHOW_SQL", dotenv.get("SPRING_JPA_SHOW_SQL"));
+        System.setProperty("JWT_SECRET", dotenv.get("JWT_SECRET"));
+    }
+}
--- src/main/java/com/api/kanban/KanbanApplicationMain.java
+++ src/main/java/com/api/kanban/KanbanApplicationMain.java
@@ -1,12 +1,18 @@
 package com.api.kanban;
 
+import com.api.kanban.Config.EnvLoader;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class KanbanApplicationMain {
 
+	static {
+		EnvLoader.loadEnv();
+	}
+
 	public static void main(String[] args) {
+
 		SpringApplication.run(KanbanApplicationMain.class, args);
 	}
 
